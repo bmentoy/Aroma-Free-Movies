3bf72210ffea305e1283d29853eed6e26d289189
Zyalix_18 <86035983+bmentoy@users.noreply.github.com>
2025-01-16 22:33:18 +0545

    Update script.js

--- script.js
+++ script.js
@@ -14,19 +14,19 @@ const movies = [
         title: "Avatar: The Way of Water", 
         image: "https://upload.wikimedia.org/wikipedia/en/5/54/Avatar_The_Way_of_Water_poster.jpg", 
         description: "Avatar: The Way of Water is a 2022 epic science fiction film. It is the sequel to Avatar (2009) and the second installment in the Avatar film series. It follows a blue-skinned humanoid Na'vi named Jake Sully (Worthington) as he and his family, under renewed human threat, seek refuge with the aquatic Metkayina clan of Pandora, a habitable exomoon on which they live.",
-        video: "path-to-avatar-video.mp4"
+        video: "https://drive.google.com/file/d/1jU2_eqK0c5Wkx5qpmAJEdUX8kTdA_sWl/view?usp=sharing"
     },
     { 
         title: "Marco", 
         image: "https://upload.wikimedia.org/wikipedia/en/b/b3/Marco_Malayalam_film.jpg", 
         description: "Marco is a 2024 Indian Malayalam-language action thriller film. The film stars Unni Mukundan in the titular role, alongside Siddique, Jagadish, Abhimanyu S Thilakan, Kabir Duhan Singh, Anson Paul and Yukti Thareja in supporting roles.",
-        video: "path-to-marco-video.mp4"
+        video: "https://drive.google.com/file/d/1SQPvtLFxCG8Or7gwOkYttHdBGfRoAEjT/view?usp=sharing"
     },
     { 
         title: "Shang-Chi and the Legend of Ten Rings", 
         image: "https://upload.wikimedia.org/wikipedia/en/7/74/Shang-Chi_and_the_Legend_of_the_Ten_Rings_poster.jpeg", 
         description: "Shang-Chi and the Legend of the Ten Rings is a 2021 American superhero film based on Marvel Comics. In the film, Shang-Chi is forced to confront his past when his father Wenwu (Leung), the leader of the Ten Rings terrorist organization, draws Shang-Chi and his sister Xialing (Zhang) into a search for a mythical village.",
-        video: "path-to-shangchi-video.mp4"
+        video: "https://drive.google.com/file/d/1kZsX5TIOo-w6QfJj4eBIzKGBh1B5_nyb/view?usp=sharing"
     },
 ];
 
