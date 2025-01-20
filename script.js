772ca8e3d760c611d76e015eba1cdcc1fcdd4862
Zyalix_18 <86035983+bmentoy@users.noreply.github.com>
2025-01-20 16:17:47 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+function toggleMenu() {
+    const menu = document.getElementById("mobile-menu");
+    menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+}
+
 // Define your movies array
 const movies = [
 {
